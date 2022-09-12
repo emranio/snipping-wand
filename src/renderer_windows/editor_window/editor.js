c20244afe4e152a3fdc0a94ab19a66d9c231275a
Emran <emranio@yahoo.com>
2022-09-12 20:54:22 +0600

    12-9/8:54

--- src/renderer_windows/editor_window/editor.js
+++ src/renderer_windows/editor_window/editor.js
@@ -3,9 +3,6 @@ import "./stylesheets/main.css";
 // Everything below is just a demo. You can delete all of it.
 
 import { ipcRenderer } from "electron";
-import jetpack from "fs-jetpack";
-import { greet } from "./hello_world/hello_world";
-import env from "env";
 
 document.querySelector("#app").style.display = "block";
 
