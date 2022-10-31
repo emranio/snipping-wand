f4c020fcbf22ba2802fc98b5c377a2203a9f42fd
Emran <emranio@yahoo.com>
2022-11-01 00:58:39 +0600

    wip

--- build/webpack.app.config.js
+++ build/webpack.app.config.js
@@ -5,12 +5,14 @@ const base = require("./webpack.base.config");
 module.exports = env => {
   return merge(base(env), {
     entry: {
-      main: "./src/main.js",
-      app: "./src/app.js"
+      main: "./src/Electron/Main.js",
+      preload: "./src/Electron/Preload.js",
+
+      // mainwindow: "./src/Renderer/Main.js",
     },
     output: {
       filename: "[name].js",
-      path: path.resolve(__dirname, "../app")
+      path: path.resolve(__dirname, "../dist")
     }
   });
 };
---
+++ dist/preload.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./src/Electron/Preload.js ***!
+  \*********************************/
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+});
+/******/ })()
+;
+//# sourceMappingURL=preload.js.map
